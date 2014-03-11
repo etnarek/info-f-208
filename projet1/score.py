80d30e8bbd327ce3c3306a5830c8ccf0566a4e88
Romain Fontaine <romain-fontaine@outlook.com>
2014-03-12 00:06:32 +0100

    [Add] Started __repr__ method for Score.

--- projet1/score.py
+++ projet1/score.py
@@ -1,6 +1,16 @@
+sortList = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*']
+
 class Score(dict):
     def __int__(self, *arg, **kw):
         dict.__init__(*arg, **kw)
+
+    def __repr__(self):
+        keys = list(self.keys())
+        #keys.sort()
+        string = ""
+        for i in filter(lambda x: x[0]==keys[0][0], keys):
+            string += i[1]
+        return string
 
     @staticmethod
     def fromFile(filename):
