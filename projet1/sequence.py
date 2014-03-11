6e12017a3d1c333130c55a32bd01122c5b9996a8
Romain Fontaine <romain-fontaine@outlook.com>
2014-03-11 21:50:48 +0100

    [Ref] Sequence classes is simpler.

--- projet1/sequence.py
+++ projet1/sequence.py
@@ -1,37 +1,13 @@
-class Sequence:
+class Sequence(list):
 
-    def __init__(self, val = ""):
-        self.sequence = val
-        self.pos = 0
-
-    def __add__(self, val):
-        self.sequence.extend(val)
-
-    def __len__(self):
-        return len(self.sequence)
-
-    def __getitem__(self, i):
-        return self.sequence[i]
-
-    def __setitem__(self, i, val):
-        self.sequence[i] = val
-
-    def __iter__(self):
-        self.pos = 0
-        return self
-
-    def __next__(self):
-        if self.pos < len(self):
-            self.pos+=1
-            return self[self.pos - 1]
-        else:
-            raise StopIteration
+    def __init__(self,*args):
+        list.__init__(self, *args)
 
     def __repr__(self):
-        return repr(self.sequence)
+        return "".join(self)
 
     def __str__(self):
-        return str(self.sequence)
+        return "".join(self)
 
     @staticmethod
     def fromFile(filename):
@@ -39,10 +15,10 @@ class Sequence:
             data = ""
             line = f.readline()
             if line[0] != '>':
-                data+=line.replace("\n","")
+                data+=line.strip("\n")
             for line in f:
                 if line[0] != '>':
-                    data+=line.replace("\n","")
+                    data+=line.strip("\n")
                 else:
                     yield Sequence(data)
                     data = ""
