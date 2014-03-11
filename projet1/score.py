59c7885e768c73cc1ce62f975feb28241bdd50a9
Romain Fontaine <romain-fontaine@outlook.com>
2014-03-12 00:07:25 +0100

    [pep8]

--- projet1/score.py
+++ projet1/score.py
@@ -1,14 +1,40 @@
-sortList = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V', 'B', 'Z', 'X', '*']
+sortList = [
+    'A',
+    'R',
+    'N',
+    'D',
+    'C',
+    'Q',
+    'E',
+    'G',
+    'H',
+    'I',
+    'L',
+    'K',
+    'M',
+    'F',
+    'P',
+    'S',
+    'T',
+    'W',
+    'Y',
+    'V',
+    'B',
+    'Z',
+    'X',
+    '*']
+
 
 class Score(dict):
+
     def __int__(self, *arg, **kw):
         dict.__init__(*arg, **kw)
 
     def __repr__(self):
         keys = list(self.keys())
-        #keys.sort()
+        # keys.sort()
         string = ""
-        for i in filter(lambda x: x[0]==keys[0][0], keys):
+        for i in filter(lambda x: x[0] == keys[0][0], keys):
             string += i[1]
         return string
 
@@ -25,4 +51,3 @@ class Score(dict):
                     for i in range(len(data)):
                         matrice[up[i], head] = data[i]
         return matrice
-
