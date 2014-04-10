b62568fe2c360b9b741696453ac04e292cc3a9e1
Romain Fontaine <romain-fontaine@outlook.com>
2014-04-10 15:32:40 +0200

    Use affine penalitu to créate matrix.

--- projet1/globalResolution.py
+++ projet1/globalResolution.py
@@ -1,34 +1,48 @@
-def createF(seq1, seq2, I, E, score):
-    F = [[0 for j in seq2] for i in seq1]
-    for i in range(len(seq1)):
-        F[i][0] = i * I
-    for i in range(len(seq2)):
-        F[0][i] = i * I
+#! /bin/python3
 
-    gap2 = 0
-    gap3 = 0
+import sys
+from sequence import Sequence
+from score import Score
+
+def createF(seq1, seq2, I, E, score):
+    matrix = [[[0, 0, 0] for j in seq2] for i in seq1]
+    for i in range(1, len(seq1)):
+        S = matrix[i-1][0][0] - I
+        V = matrix[i-1][0][1] - E
+        V = max(S, V)
+        matrix[i][0] = [V] * 3
+    for i in range(1, len(seq2)):
+        S = matrix[i-1][0][0] - I
+        W = matrix[i-1][0][2] - E
+        W = max(S, W)
+        matrix[i][0] = [W] * 3
+
     for i in range(1, len(seq1)):
         for j in range(1, len(seq2)):
-            penality2 = getPenality(gap2, I, E)
-            penality3 = getPenality(gap3, I, E)
-            choice1 = F[i-1][j-1] + score[seq1[i], seq2[j]]
-            choice2 = F[i-1][j] + penality2
-            choice3 = F[i][j - 1] + penality3
-            F[i][j] = max(choice1, choice2, choice3)
-            if choice2 > choice1:
-                penality2 += 1
-                penality3 = 0
-            elif choice3 > choice2:
-                penality3 += 1
-                penality2 = 0
-            else:
-                penality2, penality3 = 0, 0
+            S = matrix[i-1][j][0] - I
+            V = matrix[i-1][j][1] - E
+            V = max(S, V)
+            matrix[i][j][1] = V
 
+            S = matrix[i][j-1][0] - I
+            W = matrix[i][j-1][2] - E
+            W = max(S, W)
+            matrix[i][j][2] = W
 
-    return F
-
-def getPenality(nbGap, I, E):
-    return -I-nbGap*E
+            S = matrix[i-1][j-1][0] + score[seq1[i], seq2[j]]
+            S = max(S, V, W)
+            matrix[i][j][0] = S
+    return matrix
 
 def aligne(seq1, seq2, F):
     pass
+
+if __name__ == '__main__':
+    fileName = sys.argv[1]
+    score = Score.fromFile(fileName)
+    fileName = sys.argv[2]
+    seq = list(Sequence.fromFile(fileName))
+    seq1 = seq[0]
+    seq2 = seq[1]
+
+    print(createF(seq1, seq2, 6, 3, score))
