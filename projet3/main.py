fe80d204bb70f00b60be0d5cfe21bfe28390e937
Romain Fontaine <romain-fontaine@outlook.com>
2014-05-25 13:32:45 +0200

    added description.

--- projet3/main.py
+++ projet3/main.py
@@ -103,7 +103,7 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Calculate the profile (PSSM matrix) with the sequences in the sequenceFile and the probability in the probaFile. The output can be set in the outFile.")
     parser.add_argument("sequenceFilename", help="The file containing all the sequences to parse.")
     parser.add_argument("probaFilename", help="The file containing the probability for all amino acid.")
     parser.add_argument("-b", "--beta", help="Set the value to use for beta (default = sqrt(nombre de sequences).", type=float)
